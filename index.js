4b4bc46d6324278e4c3f5559b9b40418606ae534
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-06-02 20:17:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ if (name==="Susan"){
   console.log("Expected the name to be Susan")
 }
 
-if (height="70")
+if (height="70"){
+  console.log("The height is correct")
+}
 // Don't worry about this
 module.exports = { name, height
 }
